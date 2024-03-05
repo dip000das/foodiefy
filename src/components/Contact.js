ef0d750db5637dfec06acd7e59699e1f9cbf650c
Dip Kumar Das <80997532+dip000das@users.noreply.github.com>
2024-03-05 19:21:03 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,9 @@
-import React from 'react';
-
 const Contact = () => {
-  return (
-    <div>
-      <h1>Contact Us</h1>
-      <h2>This is the Contact Page</h2>
-    </div>
-  );
-};
+    return(
+        <div>
+            Contact us
+        </div>
+    )
+}
 
 export default Contact;
