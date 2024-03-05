52908d73c0d8f7bcb746396bfe5b39b5090410d7
Dip Kumar Das <80997532+dip000das@users.noreply.github.com>
2024-03-05 19:36:52 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,10 @@
 export const CDN_URL =
-  'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/';
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const LOGO_URL =
-  'https://png.pngtree.com/png-vector/20230217/ourmid/pngtree-food-logo-design-for-restaurant-and-business-png-image_6604922.png';
+export const COVID_URL =
+  "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp";
 
-// export const MENU_API =
-//   'https://corsproxy.org/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5148386&lng=88.4097881&restaurantId=';
+export const MENU_URL = "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue";
+// export const MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.0918606&lng=72.8825928&restaurantId=";
 
-
-export const MENU_API ='https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D28.6696613%26lng%3D77.0568756%26restaurantId%3D';
+export default CDN_URL;
