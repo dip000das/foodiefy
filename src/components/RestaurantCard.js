d62b5b0b48b59bda76d5de63f1a790c415799b24
Dip Kumar Das <80997532+dip000das@users.noreply.github.com>
2024-03-05 19:26:48 +0530

    Update RestaurantCard.js

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,108 +1,65 @@
-import { CDN_URL } from '../utils/constants';
-import { FiClock } from 'react-icons/fi';
-import { AiOutlineStar } from 'react-icons/ai';
-import { useContext } from 'react';
-import UserContext from '../utils/UserContext';
+import { CDN_URL } from "../utils/constants";
+import { COVID_URL } from "../utils/constants";
 
 const RestaurantCard = (props) => {
   const { resData } = props;
-  const { loggedInUser } = useContext(UserContext);
 
   const {
     cloudinaryImageId,
     name,
-    cuisines,
     avgRating,
+    aggregatedDiscountInfoV3,
+    cuisines,
     costForTwo,
     sla,
-    locality,
-    areaName,
-    aggregatedDiscountInfoV3,
   } = resData?.info;
-console.log(resData.info);
-  // Display up to 3 cuisine keywords
-  const displayedCuisines = cuisines.slice(0, 3).join(", ");
-
 
   return (
-    <div className="m-4 p-4 w-[250px] h-[410px] bg-gray-100 rounded-lg hover:shadow-md hover:bg-gray-200 transition-all  ">
-    
-      <div>
-        <div className="absolute mt-32 ml-5 font-extrabold text-stone-50 bg-gradient-to-t from-black">{aggregatedDiscountInfoV3.header} {aggregatedDiscountInfoV3.subHeader}</div>
-        <img
-          className="w-[250px] h-[150px] rounded-lg"
-          src={CDN_URL + cloudinaryImageId}
-          alt={name}
-          
-        />
-      </div>
-      
+    <div className="Food-item">
+      <div className="Food-item-margin">
+        <span className="Food-item-link">
+          <div className="card-img">
+            <img src={CDN_URL + cloudinaryImageId} alt="restaurant img" />
+          </div>
+          <div className="off">
+            <p>
+              {aggregatedDiscountInfoV3?.header
+                ? `${aggregatedDiscountInfoV3.header}${
+                    aggregatedDiscountInfoV3.subHeader
+                      ? ` ${aggregatedDiscountInfoV3.subHeader}`
+                      : ""
+                  }`
+                : "Flat Rs. 49 OFF"}
+            </p>
+          </div>
+        </span>
+        <span className="Food-item-link">
+          <div className="pname">
+            <h4>{name}</h4>
 
-      <div>
-        <h3 className="py-4 text-lg font-bold">{name}</h3>
-        <hr />
-        <h4>
-        <em>
-        
-       {displayedCuisines}
-       {cuisines.length > 3 && '... '}
-
-        </em>
-        </h4>
-        <div className="flex align-middle">
-      
-          
-          <h4> <AiOutlineStar />{avgRating} stars</h4>
-        </div>
-
-        
-
-        <h4 className="align-middle">
-          <h2><FiClock />{sla.deliveryTime} minutes</h2>
-          
-        </h4>
-        
-        <h4>{areaName}</h4>
-       
+            <div className="rating">
+              <p>{avgRating} </p>
+              <i className="fa-solid fa-star" style={{ color: "#ffffff" }} />
+            </div>
+          </div>
+          <div className="Category">
+            <p className="p1">{cuisines.join(", ")}</p>
+            <p className="p2">{costForTwo}</p>
+          </div>
+          <div className="time">
+            <p>{sla.deliveryTime} min</p>
+          </div>
+          <div className="line"></div>
+          <div className="covid">
+            <div className="covid-img">
+              <img src={COVID_URL} alt="Covid img" />
+            </div>
+            <p>Follows all Max Safety measures to ensure your food is safe</p>
+          </div>
+        </span>
       </div>
     </div>
   );
-};
-// return (
-//   <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-
-//     <img className="res-logo" src={CDN_URL + cloudinaryImageId} alt={name} />
-    
-//     <h3>{name}</h3>
-//     <h4>
-//       {displayedCuisines}
-//       {cuisines.length > 3 && '... '}
-//     </h4>
-//     <h4>{locality}</h4>
-//     <h4>{avgRating} star rating</h4>
-//     <h4>{costForTwo}</h4>
-//     <div className="orderBox">
-//       <h4>Order Now</h4>
-//     </div>
-//   </div>
-// );
-// };
-
-// * Higher Order Component
-
-// * input - RestaurantCard => RestaurantCardPromoted
-
-export const withPromotedLabel = (RestaurantCard) => {
-  return (props) => {
-    return (
-      <div>
-        <label className="absolute p-2 mb-6 ml-4 text-white bg-black rounded-lg">
-          Promoted
-        </label>
-        <RestaurantCard {...props} />
-      </div>
-    );
-  };
 };
 
 export default RestaurantCard;
