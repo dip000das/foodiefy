87519d02821cc852814821308aebeaefe3888db7
Dip Kumar Das <80997532+dip000das@users.noreply.github.com>
2024-03-05 19:21:32 +0530

    Update Error.js

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,18 +1,16 @@
-import { useRouteError } from 'react-router-dom';
+import { useRouteError } from "react-router-dom";
 
 const Error = () => {
-  const err = useRouteError();
-  console.log(err);
+    const err = useRouteError();
+    console.log(err);
 
-  return (
-    <div>
-      <h1>Oops❗</h1>
-      <h2>Something went wrong❗</h2>
-      <h3>
-        {err.status}: {err.statusText}
-      </h3>
-    </div>
-  );
-};
+    return(
+        <div>
+            <h1>Opps</h1>
+            <h2>Something went wrong</h2>
+            <h3>{err.status}: {err.statusText}</h3>
+        </div>
+    )
+}
 
 export default Error;
