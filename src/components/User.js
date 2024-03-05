a9ff59e9a81caf5fc6280eb979dc9dec701313c9
Dip Kumar Das <80997532+dip000das@users.noreply.github.com>
2024-03-05 19:29:05 +0530

    Update User.js

--- src/components/User.js
+++ src/components/User.js
@@ -1,24 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useState } from "react";
 
-const User = ({ name }) => {
-  const [count, setCount] = useState(0);
-  const [count2] = useState(1);
-
-  useEffect(() => {
-    // * API call
-    // async function getUserInfo() {
-    // }
-  }, []);
-
-  return (
-    <div className="user-card">
-      <h1>Count: {count}</h1>
-      <h1>Count2: {count2}</h1>
-      <h2>Name: {name}</h2>
-      <h3>Location: kolkata</h3>
-      <h4>Contact: @dip333</h4>
-    </div>
-  );
-};
+const User = ({name, location}) => {
+    const [count] = useState(0);
+    const [count2] = useState(2);
+    return(
+        <div className="user-card">
+            <h1>Count = {count}</h1>
+            <h1>Count2 = {count2}</h1>
+            <h2>Name: {name}</h2>
+            <h3>Location: {location}</h3>
+            <h4>Contact: @jainrithik</h4>
+        </div>
+    )
+}
 
 export default User;
