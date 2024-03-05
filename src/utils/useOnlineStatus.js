ac836b89f6f66dc815b35e65c5057ef7a0a8e013
Dip Kumar Das <80997532+dip000das@users.noreply.github.com>
2024-03-05 19:41:29 +0530

    Update useOnlineStatus.js

--- src/utils/useOnlineStatus.js
+++ src/utils/useOnlineStatus.js
@@ -1,20 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const useOnlineStatus = () => {
-  const [onlineStatus, setOnlineStatus] = useState(true);
-
-  useEffect(() => {
-    window.addEventListener('offline', () => {
-      setOnlineStatus(false);
-    });
-
-    window.addEventListener('online', () => {
-      setOnlineStatus(true);
-    });
-  }, []);
-
-  // * boolean value
-  return onlineStatus;
+	const [isOnline, setOnline] = useState(true);
+	const Onlinehandler = () => {
+		setOnline(true);
+	};
+	const Offlinehandler = () => {
+		setOnline(false);
+	};
+	useEffect(() => {
+		window.addEventListener("online", Onlinehandler);
+        window.addEventListener("offline", Offlinehandler);
+        console.log("Online handler run")
+		return () => {
+            window.removeEventListener("online", Onlinehandler);
+			window.removeEventListener("offline", Offlinehandler);
+            console.log("Online handler return")
+		};
+	}, []);
+	return isOnline;
 };
-
 export default useOnlineStatus;
