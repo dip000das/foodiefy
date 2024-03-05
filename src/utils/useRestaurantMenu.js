c61a3eb67878fe20af5cbfeaa0afc3ca3ed49826
Dip Kumar Das <80997532+dip000das@users.noreply.github.com>
2024-03-05 19:41:57 +0530

    Update useRestaurantMenu.js

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -1,21 +1,30 @@
-import { useEffect, useState } from 'react';
-import { MENU_API } from '../utils/constants';
+import { useContext, useEffect, useState } from "react";
+import { MENU_URL } from "./constants";
+import LocationContext from "./LocationContext";
 
 const useRestaurantMenu = (resId) => {
-  const [resInfo, setResInfo] = useState(null);
+  const [restInfo, setRestInfo] = useState(null);
+  const { location } = useContext(LocationContext);
 
-  // fetchdata
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(MENU_API + resId);
-    const json = await data.json();
-    setResInfo(json.data);
-  };
+    const data = await fetch(
+      MENU_URL +
+        "&lat=" +
+        location.latitude +
+        "&lng=" +
+        location.longitude +
+        "&restaurantId=" +
+        resId
+    );
 
-  return resInfo;
+    const json = await data.json();
+    setRestInfo(json.data);
+  };
+  return restInfo;
 };
 
 export default useRestaurantMenu;
