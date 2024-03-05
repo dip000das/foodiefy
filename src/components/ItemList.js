922df4e9969dda72ce147a6b29226197c3770e9d
Dip Kumar Das <80997532+dip000das@users.noreply.github.com>
2024-03-05 19:25:11 +0530

    Update ItemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,48 +1,77 @@
-import { useDispatch } from 'react-redux';
-import { addItem } from '../utils/cartSlice';
-import { CDN_URL } from '../utils/constants';
+import { useDispatch, useSelector } from "react-redux";
+import CDN_URL from "../utils/constants";
+import { addItem, decrementQuantity, incrementQuantity, updateQuantity } from "../utils/cartSlice";
 
-const ItemList = ({ items }) => {
+const ItemList = ({ items, resInfo }) => {
   const dispatch = useDispatch();
+  const cartItems = useSelector((store) => store.cart.items);
 
   const handleAddItem = (item) => {
-    // dispatch an action
-    dispatch(addItem(item));
+    dispatch(addItem({ item, resInfo }));
   };
 
+  const handleIncrement = (item) => {
+    const existingItemIndex = cartItems.findIndex(
+      (cartItem) => cartItem.card.info.id === item.card.info.id
+    );
+
+    if (existingItemIndex !== -1) {
+      // If item already exists in the cart, dispatch the incrementQuantity action
+      dispatch(incrementQuantity(existingItemIndex));
+    }
+  };
+
+  const handleDecrement = (item) => {
+    const existingItemIndex = cartItems.findIndex(
+      (cartItem) => cartItem.card.info.id === item.card.info.id
+    );
+
+    if (existingItemIndex !== -1) {
+      // If item already exists in the cart, dispatch the decrementQuantity action
+      dispatch(decrementQuantity(existingItemIndex));
+    }
+  };
+
+  // console.log(items);
   return (
-    <div>
+    <div className="category-item">
       {items.map((item) => (
-        <div
-          key={item.card.info.id}
-          className="flex justify-between p-2 m-2 text-left border-b-2"
-        >
-          <div className="w-9/12">
-            <div className="py-2">
-              <span>{item.card.info.name}</span>
-              <span>
-                - ₹
-                {item.card.info.price
-                  ? item.card.info.price / 100
-                  : item.card.info.defaultPrice / 100}
-              </span>
-            </div>
-            <p className="text-xs">{item.card.info.description}</p>
+        <div className="item-container" key={item.card.info.id}>
+          <div className="item-info">
+            {item.card.info.itemAttribute.vegClassifier === "VEG" ? (
+              <i id="veg-logo" className="fa-regular fa-circle-stop"></i>
+            ) : (
+              <i id="nonveg-logo" className="fa-regular fa-square-caret-up"></i>
+            )}
+            <h3>{item.card.info.name}</h3>
+            <h4>
+              ₹
+              {item.card.info.price
+                ? item.card.info.price / 100
+                : item.card.info.defaultPrice / 100}
+            </h4>
+            <p>{item.card.info.description}</p>
           </div>
-          <div className="w-3/12 p-4">
-            <div className="absolute">
-              <button
-                className="p-2 ml-8 mt-[90px] rounded-lg bg-black text-white shadow-lg hover:bg-white  hover:text-black transition-all duration-[.3s]"
-                onClick={() => handleAddItem(item)}
-              >
-                Add +
-              </button>
-            </div>
-            <img
-              src={CDN_URL + item.card.info.imageId}
-              alt={item.card.info.name}
-              className="w-full rounded-md"
-            />
+          <div className="item-img">
+            <img src={CDN_URL + item.card.info.imageId} />
+            {cartItems.some(
+              (cartItem) => cartItem.card.info.id === item.card.info.id
+            ) ? (
+              // If item is in the cart, show inc-dec-counter
+              <div className="inc-dec-count menu-counter">
+                <div className="minus-counter" onClick={() => handleDecrement(item)}>-</div>
+                <span>
+                  {
+                    cartItems.find(
+                      (cartItem) => cartItem.card.info.id === item.card.info.id
+                    )?.quantity
+                  }
+                </span>
+                <div className="plus-counter" onClick={() => handleIncrement(item)}>+</div></div>
+            ) : (
+              // If item is not in the cart, show ADD button
+              <button onClick={() => handleAddItem(item)}>ADD</button>
+            )}
           </div>
         </div>
       ))}
