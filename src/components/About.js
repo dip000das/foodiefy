0f65df58a410faade86b22d45d356e0c1cdc0dc9
Dip Kumar Das <80997532+dip000das@users.noreply.github.com>
2024-03-05 19:18:31 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,64 +1,32 @@
-import { Component } from 'react';
-// import User from './User';
-import UserClass from './UserClass';
-import UserContext from '../utils/UserContext';
+import { useState } from "react";
+import FAQ from "./FAQ";
+import faqData from "../utils/faqData";
 
-class About extends Component {
-  constructor(props) {
-    super(props);
-    // console.log('Parent Constructor');
-  }
+const About = () => {
+  const [showItems, setShowItems] = useState(0);
 
-  componentDidMount() {
-    // console.log('Parent Component Did Mount');
-  }
-
-  render() {
-    // console.log('Parent Render');
-
-    return (
-      <div className="about-page">
-        <h1>About Class Component</h1>
-        <div>
-          LoggedInUser
-          <UserContext.Consumer>
-            {({ loggedInUser }) => <h1>{loggedInUser}</h1>}
-          </UserContext.Consumer>
-        </div>
-        <h2>This is About Page</h2>
-        <UserClass name={'First'} location={'kolkata'} />
+  return (
+    <div className="About-container">
+      <h1>More About this project</h1>
+      <div className="acordian">
+        {faqData.map((data, index) => (
+          <FAQ
+            key={data.id}
+            showItems={index === showItems ? true : false}
+            setShowItems={() => {
+              if (index === showItems) {
+                setShowItems(null);
+              } else {
+                setShowItems(index);
+              }
+            }}
+            title={data.title}
+            description={data.description}
+          />
+        ))}
       </div>
-    );
-  }
-}
-
-// * RENDER CYCLE OF CLASS BASED COMPONENTS WHEN THE CLASS HAS TWO CHLIDREN
-
-/* 
-*  - Parent Constructor()              -- Render Phase
-*  - Parent Render()
-
-*    - First Child Constructor()
-*    - First Child Render()
-*                                      -- Render Phase
-*    - Second Child Constructor()
-*    - Second Child Render()
-
-*     <DOM UPDATED - IN SINGLE BATCH> -> Optimizes the Performance of App  -- Commit Phase
-*    - First Child ComponentDidMount()
-*    - Second Child ComponentDidMount()
-
-*  - Parent ComponentDidMount()=
-*/
-
-// const About = () => {
-//   return (
-//     <div>
-//       <h1>About Us</h1>
-//       <h2>This is About Page</h2>
-//       <UserClass name={'Vasu (class)'} location={'Badvel class'} />
-//     </div>
-//   );
-// };
+    </div>
+  );
+};
 
 export default About;
