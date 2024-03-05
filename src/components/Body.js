a81790de70fe226ad09c3a172642ba997e28af20
Dip Kumar Das <80997532+dip000das@users.noreply.github.com>
2024-03-05 19:19:02 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,166 +1,249 @@
-import { useEffect, useState, useContext, lazy } from 'react';
-import RestaurantCard, { withPromotedLabel } from './RestaurantCard';
-import { Link } from 'react-router-dom';
-import useOnlineStatus from '../utils/useOnlineStatus';
-import UserContext from '../utils/UserContext';
-import Loader from './Loader';
-import { lazy } from 'react';
-
+import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
+import OfflinePage from "./OfflinePage";
+import LocationContext from "../utils/LocationContext";
+import CityContext from "../utils/CityContext";
+import toast from "react-hot-toast";
+import noresult from "../../logos/no-results.png"
 
 const Body = () => {
-  // * React Hook -> A normal JavaScript function which is given to us by React (or) Normal JS utility functions
-  // * useState() - Super Powerful variable
-  // * useEffect() -
-
-  // * State Variable - Super Powerful variable
-  const [listOfRestaurants, setListOfRestaurants] = useState([]);
-  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
-
-  const [searchText, setSearchText] = useState('');
-
-  const RestaurantCardPromoted = withPromotedLabel(RestaurantCard);
+  const [listOfRest, setListOfRest] = useState([]);
+  const [filteredRestaurant, setfilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  let [title, setTitle] = useState("");
+  const { location } = useContext(LocationContext);
+  const { setCity } = useContext(CityContext);
 
   useEffect(() => {
     fetchData();
-  }, []);
-
-
-
-  // const fetchData = async () => {
-  //   const data = await fetch(
-  //     'https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5148386&lng=88.4097881&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
-  //   );
-
-  //   const json = await data.json();
-
-  //    console.log(json);
-  //   // * optional chaining
-  //   // setListOfRestaurants(json.data.cards[2].data.data.cards);
-  //   setListOfRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-  //   setFilteredRestaurant(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-  // };
-
-
-
-
-
+  }, [location]);
 
   const fetchData = async () => {
     const data = await fetch(
-      'https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D12.9715987%26lng%3D77.5945627%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING'
+      `https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D${location.latitude}%26lng%3D${location.longitude}`
     );
-
+    // "https://instafood.onrender.com/api/restaurants?lat=19.0918606&lng=72.8825928"
     const json = await data.json();
 
-     console.log(json);
-    // * optional chaining
-    // setListOfRestaurants(json.data.cards[2].data.data.cards);
-    setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    setFilteredRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    console.log(json);
+
+    setListOfRest(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setfilteredRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setCity(
+      json?.data?.cards[
+        json?.data?.cards.length - 1
+      ]?.card?.card?.citySlug?.toUpperCase() || ""
+    );
+
+    // Extract unique identifiers from existing objects
+    const existingIds = new Set(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants.map(
+        (rest) => rest.info.id
+      )
+    );
+
+    // Filter additional objects based on unique identifiers
+    const additionalRestaurants =
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants.filter(
+        (rest) => !existingIds.has(rest.info.id)
+      );
+
+    // Update state with additional unique objects
+    setListOfRest((prevList) => [
+      ...(prevList || []),
+      ...(additionalRestaurants || []),
+    ]);
+    setfilteredRestaurant((prevList) => [
+      ...(prevList || []),
+      ...(additionalRestaurants || []),
+    ]);
+
+    title =
+      json?.data?.cards[1]?.card?.card?.header?.title ||
+      "Top restaurant chains";
+    setTitle(title);
+
+    if (
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants === undefined
+    ) {
+      fetchMobileData();
+    }
   };
 
+  const fetchMobileData = async () => {
+    const mData = await fetch(
+      `https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fmapi%2Fhomepage%2FgetCards%3Flat%3D${location.latitude}%26lng%3D${location.longitude}`
+    );
 
+    const json = await mData.json();
 
-
-
-
-
-
+    setListOfRest(
+      json.data.success.cards[4].gridWidget.gridElements.infoWithStyle
+        .restaurants
+    );
+    setfilteredRestaurant(
+      json.data.success.cards[4].gridWidget.gridElements.infoWithStyle
+        .restaurants
+    );
+  };
 
   const onlineStatus = useOnlineStatus();
 
-  if (onlineStatus === false)
-    return (
-      <h1 style={{ textAlign: 'center', marginTop: '100px' }}>
-        Looks like you're offline! Please check your internet connection
-      </h1>
-    );
+  if (onlineStatus === false) return <OfflinePage />;
 
-  const { loggedInUser, setUserName } = useContext(UserContext);
-
-  return listOfRestaurants.length === 0 ? (
-    <Loader/>
+  return listOfRest.length === 0 ? (
+    <Shimmer />
   ) : (
     <div className="body">
-      {/* <div className="search-container">
-        <input type="text" placeholder="Search Food or Restaurant" />
-        <button>Search</button>
-      </div> */}
-      <div className="flex justify-between">
-        <div className="p-4 m-4 search">
+      <div className="filter">
+        <div className="search">
           <input
+            className="search-box"
             type="text"
-            placeholder="Search a restaurant you want..."
-            className="px-4 py-2 border border-transparent shadow-md font-medium bg-gray-100 rounded-md focus:border-0 focus:outline-0 w-[300px] placeholder:font-medium focus:border-b-2 focus:border-green-500"
+            placeholder="Search for restaurants, cuisines"
             value={searchText}
             onChange={(e) => {
-              setSearchText(e.target.value);
+              const searchTextValue = e.target.value.toLowerCase();
+              setSearchText(searchTextValue);
+
+              if (searchTextValue === "") {
+                setfilteredRestaurant(listOfRest);
+              } else {
+                const filteredList = listOfRest.filter(
+                  (res) =>
+                    res.info.name.toLowerCase().includes(searchTextValue) ||
+                    res.info.cuisines.some((cuisine) =>
+                      cuisine.toLowerCase().includes(searchTextValue)
+                    )
+                );
+                setfilteredRestaurant(filteredList);
+              }
             }}
           />
-          <button
-            className="px-4 py-2 m-4 bg-green-100 rounded-lg shadow-md hover:bg-green-300 duration-[.3s] font-medium"
+          <div
+            className="search-icon"
             onClick={() => {
-              // * Filter the restaurant cards and update the UI
-              // * searchText
               console.log(searchText);
-
-              const filteredRestaurant = listOfRestaurants.filter((res) =>
-                res.info.name.toLowerCase().includes(searchText.toLowerCase())
-              );
-
-              setFilteredRestaurant(filteredRestaurant);
+              setSearchText("");
             }}
           >
-            Search
-          </button>
+            <i
+              className="fa-solid fa-magnifying-glass"
+              style={{ color: "#939393" }}
+            ></i>
+          </div>
         </div>
-        <div className="flex items-center p-4 m-4 search">
-          <button
-            className="px-4 py-2 m-4 bg-gray-100 shadow-md hover:bg-gray-200 duration-[.3s] rounded-lg font-medium"
-            onClick={() => {
-              // * Filter logic
-              const filteredList = listOfRestaurants.filter(
-                (res) => parseFloat(res.info.avgRating) > 4
+        <button
+          className="filter-btn"
+          onClick={() => {
+            setfilteredRestaurant(listOfRest);
+            toast.success("All Restaurants Displayed");
+          }}
+        >
+          All Restaurants
+        </button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredRest = listOfRest.filter(
+              (res) => res.info.avgRating > 4.3
+            );
+            setfilteredRestaurant(filteredRest);
+            toast.success("Highly Rated Restaurants");
+          }}
+        >
+          Ratings 4.3+
+        </button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredRest = listOfRest.filter(
+              (res) => res?.info?.veg === true
+            );
+            setfilteredRestaurant(filteredRest);
+            toast.success("Showing Pure Veg Restaurants");
+          }}
+        >
+          Pure Veg
+        </button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredRest = listOfRest.filter(
+              (res) => res?.info?.sla?.deliveryTime <= 25
+            );
+            setfilteredRestaurant(filteredRest);
+            toast.success("Fast Delivery Restaurants Displayed");
+          }}
+        >
+          Fast Delivery
+        </button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredRest = listOfRest.filter((res) => {
+              const costForTwo = parseInt(
+                res?.info?.costForTwo
+                  .replace("₹", "")
+                  .replace(" for two", "")
+                  .trim()
               );
-
-              setFilteredRestaurant(filteredList);
-              console.log(filteredList);
-            }}
-          >
-            Top Rated Restaurants
-          </button>
-        </div>
-        <div className="flex items-center p-4 m-4 search">
-          <label htmlFor="name" className="font-medium">
-            User Name:{' '}
-          </label>
-          <input
-            id="name"
-            className="px-4 py-2 border border-transparent shadow-md bg-gray-100 rounded-md focus:border-0 focus:outline-0 w-[200px] ml-[20px] focus:border-b-2 focus:border-green-500"
-            value={loggedInUser}
-            onChange={(e) => setUserName(e.target.value)}
-          />
-        </div>
+              return costForTwo <= 300;
+            });
+            setfilteredRestaurant(filteredRest);
+            toast.success("Budget-Friendly Restaurants Displayed");
+          }}
+        >
+          Less than ₹300
+        </button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredRest = listOfRest.filter((res) => {
+              const costForTwo = parseInt(
+                res?.info?.costForTwo
+                  .replace("₹", "")
+                  .replace(" for two", "")
+                  .trim()
+              );
+              return costForTwo > 300 && costForTwo <= 600;
+            });
+            setfilteredRestaurant(filteredRest);
+            toast.success("Discover Dining Deals: ₹300 - ₹600");
+          }}
+        >
+          Range: ₹300 - ₹600
+        </button>
       </div>
-      <div className="flex flex-wrap justify-center">
-        {/* // * looping through the <RestaurentCard /> components Using Array.map() method */}
-
-        {filteredRestaurant.map((restaurant) => (
-          <Link
-            style={{
-              textDecoration: 'none',
-              color: '#000',
-            }}
-            key={restaurant.info.id}
-            to={'/restaurants/' + restaurant.info.id}
-          >
-            {restaurant.info.promoted ? (
-              <RestaurantCardPromoted resData={restaurant} />
-            ) : (
-              <RestaurantCard resData={restaurant} />
-            )}
-          </Link>
-        ))}
+      <div className="Food-menu">
+        <h2 className="food-menu-title">{title}</h2>
+        {filteredRestaurant.length === 0 ? (
+          <div className="no-results-found">
+            <img
+              src={noresult}
+              alt="Search Results Are Finished - No Results Found Cartoon@clipartmax.com"
+            />
+          </div>
+        ) : (
+          <div className="Food-card">
+            {filteredRestaurant.map((restaurant) => (
+              <Link
+                key={restaurant.info.id}
+                to={"/restaurants/" + restaurant.info.id}
+              >
+                <RestaurantCard resData={restaurant} />
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
